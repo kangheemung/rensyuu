26a18b720b243b5c7162ab051793e9a0ceb342da
EC2 Default User <ec2-user@ip-10-0-0-228.ap-northeast-1.compute.internal>
2023-11-15 15:01:00 +0000

    edit controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,25 @@
-class Api::V1::UsersController < ApplicationController
+class Api::V1::UsersController <  ApplicationController
+    skip_before_action :jwt_authenticate, only: [:create]
+    def create
+        user=User.new(user_params)
+        p"=============new======"
+        p params
+        p"==================="
+        p "user variable"
+        
+        if user.save
+            p"=============new======"
+            p params
+            p"==================="
+            p "user variable"
+           token=encode(user.id)
+           render json: {status: 201, data: {name: user.name, email: user.email, token: token}}
+        else   
+           render json:{status: 400, error: "user can't save"}
+        end   
+    end
+    private
+    def user_params
+        params.require(:user).permit(:name,:email,:password, :password_confirmation)
+    end
 end
